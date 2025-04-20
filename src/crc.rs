d9d05a8fb726e4fb9173fcf751e65ec5b0648444
Takashiidobe <idobetakashi@gmail.com>
2025-04-20 19:24:50 -0400

    add docs for crc

--- src/crc.rs
+++ src/crc.rs
@@ -1,8 +1,11 @@
 use const_for::const_for;
 use gf256::p64;
 
+/// The CRC-32 polynomial used in this implementation.
+/// This is the standard polynomial `0x104C11DB7` used in Ethernet, ZIP, and other applications.
 const POLYNOMIAL: p64 = p64(0x104c11db7);
 
+/// A lookup table for fast CRC-32 computation.
 const CRC_TABLE: [u32; 256] = {
     let mut table = [0; 256];
     const_for!(i in 0..table.len() => {
@@ -14,6 +17,25 @@ const CRC_TABLE: [u32; 256] = {
     table
 };
 
+/// Computes the CRC-32 checksum of the given data slice.
+///
+/// # Arguments
+///
+/// * `data` - A slice of bytes for which the CRC-32 checksum is to be calculated.
+///
+/// # Returns
+///
+/// The computed CRC-32 checksum as a `u32`.
+///
+/// # Example
+///
+/// ```
+/// use crypto::crc::crc32;
+/// 
+/// let input = b"Hello World!";
+/// let checksum = crc32(input);
+/// assert_eq!(checksum, 0x1c291ca3);
+/// ```
 pub fn crc32(data: &[u8]) -> u32 {
     let mut crc = 0xffffffff;
 
